dacbb0c6f0ad8cfcc61d21fd038104bc78c125df
damia <damia@DESKTOP-6RSQSSG>
2021-05-22 20:05:35 +0200

    edgeTraversed

--- src/main/java/it/polito/tdp/metroparis/model/Model.java
+++ src/main/java/it/polito/tdp/metroparis/model/Model.java
@@ -1,11 +1,17 @@
 package it.polito.tdp.metroparis.model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.jgrapht.Graph;
 import org.jgrapht.Graphs;
+import org.jgrapht.event.ConnectedComponentTraversalEvent;
+import org.jgrapht.event.EdgeTraversalEvent;
+import org.jgrapht.event.TraversalListener;
+import org.jgrapht.event.VertexTraversalEvent;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.SimpleGraph;
 import org.jgrapht.traverse.BreadthFirstIterator;
@@ -15,6 +21,7 @@ import it.polito.tdp.metroparis.db.MetroDAO;
 public class Model {
 	
 	Graph<Fermata, DefaultEdge> grafo ;
+	Map<Fermata,Fermata>predecessore;
 
 	public void creaGrafo() {
 		this.grafo = new SimpleGraph<>(DefaultEdge.class) ;
@@ -70,6 +77,55 @@ public class Model {
 	public List<Fermata> fermateRaggiungibili(Fermata partenza){
 		BreadthFirstIterator<Fermata, DefaultEdge> bfv= new BreadthFirstIterator<>(this.grafo,partenza);
 		
+		this.predecessore=new HashMap<>();
+		predecessore.put(partenza, null);
+		
+		bfv.addTraversalListener(new TraversalListener<Fermata,DefaultEdge>(){
+
+			@Override
+			public void connectedComponentFinished(ConnectedComponentTraversalEvent e) {
+				
+				
+			}
+
+			@Override
+			public void connectedComponentStarted(ConnectedComponentTraversalEvent e) {
+				
+				
+			}
+
+			@Override
+			public void edgeTraversed(EdgeTraversalEvent<DefaultEdge> e) {
+				DefaultEdge arco= e.getEdge();
+				Fermata a =grafo.getEdgeSource(arco);
+				Fermata b =grafo.getEdgeTarget(arco);
+				//ho scoperto a arrivando da b(se b lo conoscevo già)
+				if(predecessore.containsKey(b) && !predecessore.containsKey(a)) {
+					predecessore.put(a, b);
+					//System.out.println(a+ "scoperto da:" +b);
+				}else if(predecessore.containsKey(a) && !predecessore.containsKey(b)){
+					predecessore.put(b, a);
+					//System.out.println(b+ "scoperto da:" +a);
+				}
+				
+			}
+
+			@Override
+			public void vertexTraversed(VertexTraversalEvent<Fermata> e) {
+				/*System.out.println(e.getVertex());
+				Fermata nuova= e.getVertex();
+			    Fermata precedente=;// vertice adiacente a nuova che sia raggiunto(cioè è gia presente nelle keys della mappa
+			    predecessore.put(nuova,precedente);*/
+			}
+
+			@Override
+			public void vertexFinished(VertexTraversalEvent<Fermata> e) {
+				
+				
+			}
+			
+		});
+		
 		List<Fermata> result = new ArrayList<Fermata>();
 		
 		
@@ -89,5 +145,19 @@ public class Model {
 			
 		}
 		return null;
+	}
+	
+	public List<Fermata> trovaCammino(Fermata partenza, Fermata arrivo){
+		fermateRaggiungibili(partenza);
+		
+		List<Fermata>result=new ArrayList<>();
+		result.add(arrivo);
+		Fermata f = arrivo;
+		
+		while(predecessore.get(f)!=null) {
+			f=predecessore.get(f);
+			result.add(f);
+		}
+		return result;
 	}
 }
